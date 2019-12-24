65d82ea677894f526b122fb1f329f1a41d5f9696
David Kopala <kopala.david@gmail.com>
2019-12-23 23:13:02 -0700

    Trying again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CI/CD Testing!
+          <code>CI/CD</code> Testing Part 2!
         </p>
         <a
           className="App-link"
