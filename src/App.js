2662500df8c475cb36e7732e7e78ea807017aab8
David Kopala <kopala.david@gmail.com>
2019-12-23 23:30:26 -0700

    Part 4

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>CI/CD</code> Testing Part 3!
+          <code>CI/CD</code> Testing Part 4!
         </p>
         <a
           className="App-link"
